a6d3e0de6441d53a4b8e487075eef1712a586b65
YassineZhang <57878778@qq.com>
2024-06-21 18:33:52 +0800

    chore: bingo~

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="header">Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer height="200px">Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+<style scoped>
+.header {
+  box-shadow: 0 2px 16px rgba(54, 56, 79, 0.06);
+}
+</style>
